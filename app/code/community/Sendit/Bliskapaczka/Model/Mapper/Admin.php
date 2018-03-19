b557a1686811c867c35f4c64be0ada69248eab23
Mateusz Koszutowski <mkoszutowski@divante.pl>
2018-03-19 15:26:37 +0100

    Validation in admin panel

---
+++ app/code/community/Sendit/Bliskapaczka/Model/Mapper/Admin.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * Class to map module config to data for validator
+ */
+class Sendit_Bliskapaczka_Model_Mapper_Admin
+{
+    /**
+     * Prepare mapped data for Bliskapaczka API
+     *
+     * @param array $postData
+     * @param Sendit_Bliskapaczka_Helper_Data $helper
+     * @return array
+     */
+    public function getData(array $postData, Sendit_Bliskapaczka_Helper_Data $helper)
+    {
+        $data = [];
+
+        $data['senderEmail'] = $postData['fields']['sender_email']['value'];
+        $data['senderFirstName'] = $postData['fields']['sender_first_name']['value'];
+        $data['senderLastName'] = $postData['fields']['sender_last_name']['value'];
+        $data['senderPhoneNumber'] = $helper
+        								->telephoneNumberCleaning($postData['fields']['sender_phone_number']['value']);
+        $data['senderStreet'] = $postData['fields']['sender_street']['value'];
+        $data['senderBuildingNumber'] = $postData['fields']['sender_building_number']['value'];
+        $data['senderFlatNumber'] = $postData['fields']['sender_flat_number']['value'];
+        $data['senderPostCode'] = $postData['fields']['sender_post_code']['value'];
+        $data['senderCity'] = $postData['fields']['sender_city']['value'];
+
+        return $data;
+    }
+}
---
+++ app/code/community/Sendit/Bliskapaczka/vendor/bliskapaczkapl/bliskapaczka-api-client/src/Bliskapaczka/ApiClient/Validator/Order/Advice/Sender.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Bliskapaczka\ApiClient\Validator\Order\Advice;
+
+use Bliskapaczka\ApiClient\Validator\Order\Advice;
+use Bliskapaczka\ApiClient\AbstractValidator;
+use Bliskapaczka\ApiClient\ValidatorInterface;
+use Bliskapaczka\ApiClient\Exception;
+
+/**
+ * Sender Data Validator class
+ *
+ * @author  Mateusz Koszutowski (mkoszutowski@divante.pl)
+ * @version 0.1.0
+ */
+class Sender extends Advice implements ValidatorInterface
+{
+    protected $properties = [
+        'senderEmail' => ['maxlength' => 60, 'notblank' => true],
+        'senderPhoneNumber' => ['notblank' => true],
+        'senderPostCode' => ['notblank' => true],
+        'senderFirstName' => ['maxlength' => 30, 'notblank' => true],
+        'senderLastName' => ['maxlength' => 30, 'notblank' => true],
+        'senderStreet' => ['maxlength' => 30, 'notblank' => true],
+        'senderBuildingNumber' => ['maxlength' => 10, 'notblank' => true],
+        'senderFlatNumber' => ['maxlength' => 10],
+        'senderCity' => ['maxlength' => 30, 'notblank' => true],
+    ];
+}

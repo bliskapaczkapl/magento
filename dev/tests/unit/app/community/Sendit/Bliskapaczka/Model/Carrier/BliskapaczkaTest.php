f8a9e3721edb44f027a5428df56130f1a4996223
Mateusz Koszutowski <mkoszutowski@divante.pl>
2018-10-29 12:04:54 +0100

    Fixing cart rules for aggregated shipping method

--- dev/tests/unit/app/community/Sendit/Bliskapaczka/Model/Carrier/BliskapaczkaTest.php
+++ dev/tests/unit/app/community/Sendit/Bliskapaczka/Model/Carrier/BliskapaczkaTest.php
@@ -13,12 +13,28 @@ class BliskapaczkaTest extends TestCase
 
     protected function setUp()
     {
-        $this->request = $this->getMockBuilder(Mage_Shipping_Model_Rate_Request::class)
+
+        $this->quoteItem = $this->getMockBuilder(Mage_Sales_Model_Quote_Item::class)
                                      ->disableOriginalConstructor()
                                      ->disableOriginalClone()
                                      ->disableArgumentCloning()
                                      ->disallowMockingUnknownTypes()
                                      ->getMock();
+
+        $this->request = $this->getMockBuilder(Mage_Shipping_Model_Rate_Request::class)
+                                     ->disableOriginalConstructor()
+                                     ->disableOriginalClone()
+                                     ->disableArgumentCloning()
+                                     ->disallowMockingUnknownTypes()
+                                     ->setMethods(
+                                        array(
+                                            'getAllItems',
+                                            'getPackageQty',
+                                            'getFreeBoxes'
+                                        )
+                                    )
+                                     ->getMock();
+        $this->request->method('getAllItems')->willReturn(array($this->quoteItem));
 
         $this->helper = $this->getMockBuilder(Sendit_Bliskapaczka_Helper_Data::class)
                                      ->disableOriginalConstructor()
@@ -93,5 +109,192 @@ class BliskapaczkaTest extends TestCase
             $poczta,
             $inpost
         );
+    }
+
+    protected function getOperator()
+    {
+        $price = new StdClass();
+        $price->gross = 10.0;
+        $operator = new StdClass();
+        $operator->operatorName = 'DPD';
+        $operator->operatorFullName = 'DPD';
+        $operator->price = $price;
+
+        return $operator;
+    }
+
+    protected static function getMethod($name) {
+        $class = new ReflectionClass(Sendit_Bliskapaczka_Model_Carrier_Bliskapaczka::class);
+        $method = $class->getMethod($name);
+        $method->setAccessible(true);
+        return $method;
+    }
+
+    public function testFreeShippingWithSingleCartRuleCondition() {
+        $ruleConditionCombine = $this->getMockBuilder(Mage_SalesRule_Model_Rule_Condition_Combine::class)
+                                     ->disableOriginalConstructor()
+                                     ->disableOriginalClone()
+                                     ->disableArgumentCloning()
+                                     ->disallowMockingUnknownTypes()
+                                     ->setMethods(
+                                        array(
+                                            'validate'
+                                        )
+                                    )
+                                     ->getMock();
+        $ruleConditionCombine->method('validate')->with()->will($this->returnValue(true));
+
+        $rule = $this->getMockBuilder(Mage_SalesRule_Model_Resource_Rule::class)
+                                     ->disableOriginalConstructor()
+                                     ->disableOriginalClone()
+                                     ->disableArgumentCloning()
+                                     ->disallowMockingUnknownTypes()
+                                     ->setMethods(
+                                        array(
+                                            'getConditionsSerialized',
+                                            'setConditionsSerialized',
+                                            'getConditions'
+                                        )
+                                    )
+                                    ->getMock();
+        $rule->method('getConditionsSerialized')->will($this->returnValue(
+            array(
+                'conditions' => array()
+            )
+        ));
+        $rule->method('getConditions')->willReturn($ruleConditionCombine);
+
+        $rulesCollection = $this->getMockBuilder(Mage_SalesRule_Model_Resource_Rule_Collection::class)
+                                     ->disableOriginalConstructor()
+                                     ->disableOriginalClone()
+                                     ->disableArgumentCloning()
+                                     ->disallowMockingUnknownTypes()
+                                     ->setMethods(array('getIterator'))
+                                     ->getMock();
+        $rulesCollection->method('getIterator')->willReturn(new \ArrayObject(array($rule)));
+
+        $rules = array('single' => $rulesCollection);
+
+        $this->request->method('getPackageQty')->will($this->returnValue(true));
+        $this->request->method('getFreeBoxes')->will($this->returnValue(false));
+
+        $shippingPrice = self::getMethod('_shippingPrice');
+        $bliskapaczka = new Sendit_Bliskapaczka_Model_Carrier_Bliskapaczka();
+
+        $price = $shippingPrice->invokeArgs($bliskapaczka, array($this->getOperator(), $this->request, $rules, false));
+
+        $this->assertEquals(0.00, $price);
+    }
+
+    public function testFreeShippingWithoutAnySingleCartRuleCondition() {
+        $ruleConditionCombine = $this->getMockBuilder(Mage_SalesRule_Model_Rule_Condition_Combine::class)
+                                     ->disableOriginalConstructor()
+                                     ->disableOriginalClone()
+                                     ->disableArgumentCloning()
+                                     ->disallowMockingUnknownTypes()
+                                     ->setMethods(
+                                        array(
+                                            'validate'
+                                        )
+                                    )
+                                     ->getMock();
+        $ruleConditionCombine->method('validate')->with()->will($this->returnValue(false));
+
+        $rule = $this->getMockBuilder(Mage_SalesRule_Model_Resource_Rule::class)
+                                     ->disableOriginalConstructor()
+                                     ->disableOriginalClone()
+                                     ->disableArgumentCloning()
+                                     ->disallowMockingUnknownTypes()
+                                     ->setMethods(
+                                        array(
+                                            'getConditionsSerialized',
+                                            'setConditionsSerialized',
+                                            'getConditions'
+                                        )
+                                    )
+                                    ->getMock();
+        $rule->method('getConditionsSerialized')->will($this->returnValue(
+            array(
+                'conditions' => array()
+            )
+        ));
+        $rule->method('getConditions')->willReturn($ruleConditionCombine);
+
+        $rulesCollection = $this->getMockBuilder(Mage_SalesRule_Model_Resource_Rule_Collection::class)
+                                     ->disableOriginalConstructor()
+                                     ->disableOriginalClone()
+                                     ->disableArgumentCloning()
+                                     ->disallowMockingUnknownTypes()
+                                     ->setMethods(array('getIterator'))
+                                     ->getMock();
+        $rulesCollection->method('getIterator')->willReturn(new \ArrayObject(array($rule)));
+
+        $rules = array('single' => $rulesCollection);
+
+        $this->request->method('getPackageQty')->will($this->returnValue(true));
+        $this->request->method('getFreeBoxes')->will($this->returnValue(false));
+
+        $shippingPrice = self::getMethod('_shippingPrice');
+        $bliskapaczka = new Sendit_Bliskapaczka_Model_Carrier_Bliskapaczka();
+
+        $price = $shippingPrice->invokeArgs($bliskapaczka, array($this->getOperator(), $this->request, $rules, false));
+
+        $this->assertEquals(10, $price);
+    }
+
+    public function testFreeShippingWithAggregatedCartRuleCondition() {
+        $ruleConditionCombine = $this->getMockBuilder(Mage_SalesRule_Model_Rule_Condition_Combine::class)
+                                     ->disableOriginalConstructor()
+                                     ->disableOriginalClone()
+                                     ->disableArgumentCloning()
+                                     ->disallowMockingUnknownTypes()
+                                     ->setMethods(
+                                        array(
+                                            'validate'
+                                        )
+                                    )
+                                     ->getMock();
+        $ruleConditionCombine->method('validate')->with()->will($this->returnValue(true));
+
+        $rule = $this->getMockBuilder(Mage_SalesRule_Model_Resource_Rule::class)
+                                     ->disableOriginalConstructor()
+                                     ->disableOriginalClone()
+                                     ->disableArgumentCloning()
+                                     ->disallowMockingUnknownTypes()
+                                     ->setMethods(
+                                        array(
+                                            'getConditionsSerialized',
+                                            'setConditionsSerialized',
+                                            'getConditions'
+                                        )
+                                    )
+                                    ->getMock();
+        $rule->method('getConditionsSerialized')->will($this->returnValue(
+            array(
+                'conditions' => array()
+            )
+        ));
+        $rule->method('getConditions')->willReturn($ruleConditionCombine);
+
+        $rulesCollection = $this->getMockBuilder(Mage_SalesRule_Model_Resource_Rule_Collection::class)
+                                     ->disableOriginalConstructor()
+                                     ->disableOriginalClone()
+                                     ->disableArgumentCloning()
+                                     ->disallowMockingUnknownTypes()
+                                     ->setMethods(array('getIterator'))
+                                     ->getMock();
+        $rulesCollection->method('getIterator')->willReturn(new \ArrayObject(array($rule)));
+
+        $rules = array('aggregated' => $rulesCollection);
+
+        $this->request->method('getPackageQty')->will($this->returnValue(true));
+        $this->request->method('getFreeBoxes')->will($this->returnValue(false));
+
+        $shippingPrice = self::getMethod('_shippingPrice');
+        $bliskapaczka = new Sendit_Bliskapaczka_Model_Carrier_Bliskapaczka();
+
+        $price = $shippingPrice->invokeArgs($bliskapaczka, array($this->getOperator(), $this->request, $rules, false));
+
+        $this->assertEquals(0.00, $price);
     }
 }

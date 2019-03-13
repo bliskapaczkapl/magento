f58b4711685bafe175c8c766e8feed96d024ab67
Mateusz Koszutowski <mkoszutowski@divante.pl>
2019-03-13 11:14:44 +0100

    Manage allowed countries in courier configuration

--- app/code/community/Sendit/Bliskapaczka/vendor/composer/autoload_real.php
+++ app/code/community/Sendit/Bliskapaczka/vendor/composer/autoload_real.php
@@ -23,7 +23,7 @@ class ComposerAutoloaderInit50774287f4393a6b0c9ae6d96f2323cc
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInit50774287f4393a6b0c9ae6d96f2323cc', 'loadClassLoader'));
 
-        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
+        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION');
         if ($useStaticLoader) {
             require_once __DIR__ . '/autoload_static.php';
 
